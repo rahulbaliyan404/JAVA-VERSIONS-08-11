34a4d9a8ead1c4acd40028f34d6c1a049b17a600
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2025-12-18 14:57:03 +0530

    First commit

---
+++ src/main/java/com/home/JavaVersions0811Application.java
@@ -0,0 +1,14 @@
+package com.home;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+public class JavaVersions0811Application {
+
+	public static void main(String[] args) {
+
+//		SpringApplication.run(JavaVersions0811Application.class, args);
+	}
+
+}
